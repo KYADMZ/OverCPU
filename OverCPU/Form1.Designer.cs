b452dcea924a7a046c3d724a751d43c2b23238c6
KYADMZ <DMz210@outlook.com>
2025-03-19 19:49:52 -0600

    primer avance

--- OverCPU/Form1.Designer.cs
+++ OverCPU/Form1.Designer.cs
@@ -28,18 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            lblCPU = new Label();
+            lblRAM = new Label();
+            lblDisco = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
             SuspendLayout();
+            // 
+            // lblCPU
+            // 
+            lblCPU.AutoSize = true;
+            lblCPU.Location = new Point(100, 49);
+            lblCPU.Name = "lblCPU";
+            lblCPU.Size = new Size(38, 15);
+            lblCPU.TabIndex = 0;
+            lblCPU.Text = "label1";
+            // 
+            // lblRAM
+            // 
+            lblRAM.AutoSize = true;
+            lblRAM.Location = new Point(291, 50);
+            lblRAM.Name = "lblRAM";
+            lblRAM.Size = new Size(38, 15);
+            lblRAM.TabIndex = 1;
+            lblRAM.Text = "label2";
+            // 
+            // lblDisco
+            // 
+            lblDisco.AutoSize = true;
+            lblDisco.Location = new Point(439, 60);
+            lblDisco.Name = "lblDisco";
+            lblDisco.Size = new Size(38, 15);
+            lblDisco.TabIndex = 2;
+            lblDisco.Text = "label3";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1016, 467);
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblDisco);
+            Controls.Add(lblRAM);
+            Controls.Add(lblCPU);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblCPU;
+        private Label lblRAM;
+        private Label lblDisco;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- OverCPU/Form1.cs
+++ OverCPU/Form1.cs
@@ -1,10 +1,34 @@
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
 namespace OverCPU
+
 {
     public partial class Form1 : Form
     {
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter ramCounter;
+
         public Form1()
         {
             InitializeComponent();
+            InitializePerformanceCounters();
+        }
+
+        private void InitializePerformanceCounters()
+        {
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            timer1.Interval = 1000; // Se actualiza cada segundo
+            timer1.Tick += new EventHandler(UpdatePerformanceData);
+            timer1.Start();
+        }
+
+        private void UpdatePerformanceData(object sender, EventArgs e)
+        {
+            lblCPU.Text = $"CPU: {cpuCounter.NextValue():0.00}%";
+            lblRAM.Text = $"RAM Disponible: {ramCounter.NextValue()} MB";
         }
     }
 }
