a8bfa4095eb62dbb22c55181d6694320b992b523
JesusPalacios24 <jpalaciosmendias0@gmail.com>
2025-04-02 15:48:03 -0600

    cambios

--- OverCPU/Form1.Designer.cs
+++ OverCPU/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,18 +15,17 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
             lblCPU = new Label();
             lblRAM = new Label();
-            lblDisco = new Label();
+            lblDisk = new Label();
+            lblDiskRead = new Label();
+            lblDiskWrite = new Label();
             timer1 = new System.Windows.Forms.Timer(components);
             SuspendLayout();
+
             // 
             // lblCPU
             // 
@@ -42,7 +34,8 @@
             lblCPU.Name = "lblCPU";
             lblCPU.Size = new Size(38, 15);
             lblCPU.TabIndex = 0;
-            lblCPU.Text = "label1";
+            lblCPU.Text = "CPU: 0%";
+
             // 
             // lblRAM
             // 
@@ -51,27 +44,51 @@
             lblRAM.Name = "lblRAM";
             lblRAM.Size = new Size(38, 15);
             lblRAM.TabIndex = 1;
-            lblRAM.Text = "label2";
+            lblRAM.Text = "RAM: 0 MB";
+
             // 
-            // lblDisco
+            // lblDisk
             // 
-            lblDisco.AutoSize = true;
-            lblDisco.Location = new Point(439, 60);
-            lblDisco.Name = "lblDisco";
-            lblDisco.Size = new Size(38, 15);
-            lblDisco.TabIndex = 2;
-            lblDisco.Text = "label3";
+            lblDisk.AutoSize = true;
+            lblDisk.Location = new Point(491, 50);
+            lblDisk.Name = "lblDisk";
+            lblDisk.Size = new Size(38, 15);
+            lblDisk.TabIndex = 2;
+            lblDisk.Text = "Disco: 0%";
+
+            // 
+            // lblDiskRead
+            // 
+            lblDiskRead.AutoSize = true;
+            lblDiskRead.Location = new Point(491, 70);
+            lblDiskRead.Name = "lblDiskRead";
+            lblDiskRead.Size = new Size(100, 15);
+            lblDiskRead.TabIndex = 3;
+            lblDiskRead.Text = "Lectura: 0 MB/s";
+
+            // 
+            // lblDiskWrite
+            // 
+            lblDiskWrite.AutoSize = true;
+            lblDiskWrite.Location = new Point(491, 90);
+            lblDiskWrite.Name = "lblDiskWrite";
+            lblDiskWrite.Size = new Size(100, 15);
+            lblDiskWrite.TabIndex = 4;
+            lblDiskWrite.Text = "Escritura: 0 MB/s";
+
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(lblDisco);
+            Controls.Add(lblDisk);
             Controls.Add(lblRAM);
             Controls.Add(lblCPU);
+            Controls.Add(lblDiskRead);
+            Controls.Add(lblDiskWrite);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Monitor de Recursos";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -80,7 +97,9 @@
 
         private Label lblCPU;
         private Label lblRAM;
-        private Label lblDisco;
+        private Label lblDisk;
+        private Label lblDiskRead;
+        private Label lblDiskWrite;
         private System.Windows.Forms.Timer timer1;
     }
 }
--- OverCPU/Form1.cs
+++ OverCPU/Form1.cs
@@ -3,12 +3,14 @@ using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace OverCPU
-
 {
     public partial class Form1 : Form
     {
         private PerformanceCounter cpuCounter;
         private PerformanceCounter ramCounter;
+        private PerformanceCounter diskCounter;
+        private PerformanceCounter diskReadCounter;
+        private PerformanceCounter diskWriteCounter;
 
         public Form1()
         {
@@ -20,6 +22,10 @@ namespace OverCPU
         {
             cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
             ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            diskCounter = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
+            diskReadCounter = new PerformanceCounter("PhysicalDisk", "Disk Read Bytes/sec", "_Total");
+            diskWriteCounter = new PerformanceCounter("PhysicalDisk", "Disk Write Bytes/sec", "_Total");
+
             timer1.Interval = 1000; // Se actualiza cada segundo
             timer1.Tick += new EventHandler(UpdatePerformanceData);
             timer1.Start();
@@ -27,8 +33,14 @@ namespace OverCPU
 
         private void UpdatePerformanceData(object sender, EventArgs e)
         {
+            float diskReadMB = diskReadCounter.NextValue() / (1024 * 1024); // Convertir bytes a MB
+            float diskWriteMB = diskWriteCounter.NextValue() / (1024 * 1024); // Convertir bytes a MB
+
             lblCPU.Text = $"CPU: {cpuCounter.NextValue():0.00}%";
             lblRAM.Text = $"RAM Disponible: {ramCounter.NextValue()} MB";
+            lblDisk.Text = $"Disco: {diskCounter.NextValue():0.00}%";
+            lblDiskRead.Text = $"Lectura Disco: {diskReadMB:0.00} MB/s";
+            lblDiskWrite.Text = $"Escritura Disco: {diskWriteMB:0.00} MB/s";
         }
     }
 }
