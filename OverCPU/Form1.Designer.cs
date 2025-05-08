d6939314475f9c957a3a4baa2568842406d7b4e2
JesusPalacios24 <jpalaciosmendias0@gmail.com>
2025-05-07 21:48:12 -0600

    cambios

--- OverCPU/Form1.Designer.cs
+++ OverCPU/Form1.Designer.cs
@@ -1,6 +1,11 @@
-﻿namespace OverCPU
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OverCPU
 {
-    partial class Form1
+    partial class Form1 : Form
     {
         private System.ComponentModel.IContainer components = null;
 
@@ -17,208 +22,182 @@
 
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            lblCPU = new Label();
-            lblRAM = new Label();
-            lblDisk = new Label();
-            lblDiskRead = new Label();
-            lblDiskWrite = new Label();
-            timer1 = new System.Windows.Forms.Timer(components);
-            panel1 = new Panel();
-            lblCPUN = new Label();
-            progressBarcpu = new ProgressBar();
-            panel2 = new Panel();
-            lbltotalram = new Label();
-            labelrampercent = new Label();
-            progressBarRAM = new ProgressBar();
-            panel3 = new Panel();
-            label1 = new Label();
-            lbldiskn = new Label();
-            panel1.SuspendLayout();
-            panel2.SuspendLayout();
-            panel3.SuspendLayout();
-            SuspendLayout();
-            // 
-            // lblCPU
-            // 
-            lblCPU.AutoSize = true;
-            lblCPU.Location = new Point(19, 25);
-            lblCPU.Name = "lblCPU";
-            lblCPU.Size = new Size(52, 15);
-            lblCPU.TabIndex = 0;
-            lblCPU.Text = "CPU: 0%";
-            // 
-            // lblRAM
-            // 
-            lblRAM.AutoSize = true;
-            lblRAM.Location = new Point(19, 10);
-            lblRAM.Name = "lblRAM";
-            lblRAM.Size = new Size(66, 15);
-            lblRAM.TabIndex = 1;
-            lblRAM.Text = "RAM: 0 MB";
-            // 
-            // lblDisk
-            // 
-            lblDisk.AutoSize = true;
-            lblDisk.Location = new Point(20, 10);
-            lblDisk.Name = "lblDisk";
-            lblDisk.Size = new Size(58, 15);
-            lblDisk.TabIndex = 2;
-            lblDisk.Text = "Disco: 0%";
-            // 
-            // lblDiskRead
-            // 
-            lblDiskRead.AutoSize = true;
-            lblDiskRead.Location = new Point(20, 25);
-            lblDiskRead.Name = "lblDiskRead";
-            lblDiskRead.Size = new Size(89, 15);
-            lblDiskRead.TabIndex = 3;
-            lblDiskRead.Text = "Lectura: 0 MB/s";
-            // 
-            // lblDiskWrite
-            // 
-            lblDiskWrite.AutoSize = true;
-            lblDiskWrite.Location = new Point(20, 40);
-            lblDiskWrite.Name = "lblDiskWrite";
-            lblDiskWrite.Size = new Size(95, 15);
-            lblDiskWrite.TabIndex = 4;
-            lblDiskWrite.Text = "Escritura: 0 MB/s";
-            // 
-            // panel1
-            // 
-            panel1.BackColor = SystemColors.ControlLight;
-            panel1.BorderStyle = BorderStyle.Fixed3D;
-            panel1.Controls.Add(lblCPUN);
-            panel1.Controls.Add(progressBarcpu);
-            panel1.Controls.Add(lblCPU);
-            panel1.Location = new Point(12, 12);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(237, 98);
-            panel1.TabIndex = 5;
-            // 
-            // lblCPUN
-            // 
-            lblCPUN.AutoSize = true;
-            lblCPUN.Location = new Point(3, 0);
-            lblCPUN.Name = "lblCPUN";
-            lblCPUN.Size = new Size(27, 15);
-            lblCPUN.TabIndex = 2;
-            lblCPUN.Text = "cpu";
-            // 
-            // progressBarcpu
-            // 
-            progressBarcpu.Location = new Point(19, 49);
-            progressBarcpu.Name = "progressBarcpu";
-            progressBarcpu.Size = new Size(174, 23);
-            progressBarcpu.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = SystemColors.ControlLight;
-            panel2.BorderStyle = BorderStyle.Fixed3D;
-            panel2.Controls.Add(lbltotalram);
-            panel2.Controls.Add(labelrampercent);
-            panel2.Controls.Add(progressBarRAM);
-            panel2.Controls.Add(lblRAM);
-            panel2.Location = new Point(255, 12);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(230, 98);
-            panel2.TabIndex = 6;
-            // 
-            // lbltotalram
-            // 
-            lbltotalram.AutoSize = true;
-            lbltotalram.Location = new Point(19, 30);
-            lbltotalram.Name = "lbltotalram";
-            lbltotalram.Size = new Size(33, 15);
-            lbltotalram.TabIndex = 4;
-            lbltotalram.Text = "RAM";
-            // 
-            // labelrampercent
-            // 
-            labelrampercent.AutoSize = true;
-            labelrampercent.Location = new Point(176, 10);
-            labelrampercent.Name = "labelrampercent";
-            labelrampercent.Size = new Size(38, 15);
-            labelrampercent.TabIndex = 3;
-            labelrampercent.Text = "0.00%";
-            // 
-            // progressBarRAM
-            // 
-            progressBarRAM.Location = new Point(19, 49);
-            progressBarRAM.Name = "progressBarRAM";
-            progressBarRAM.Size = new Size(181, 23);
-            progressBarRAM.TabIndex = 2;
-            // 
-            // panel3
-            // 
-            panel3.BackColor = SystemColors.ControlLight;
-            panel3.BorderStyle = BorderStyle.Fixed3D;
-            panel3.Controls.Add(label1);
-            panel3.Controls.Add(lbldiskn);
-            panel3.Controls.Add(lblDisk);
-            panel3.Controls.Add(lblDiskRead);
-            panel3.Controls.Add(lblDiskWrite);
-            panel3.Location = new Point(491, 12);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(264, 98);
-            panel3.TabIndex = 7;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.BorderStyle = BorderStyle.FixedSingle;
-            label1.Location = new Point(20, 60);
-            label1.Name = "label1";
-            label1.Size = new Size(53, 17);
-            label1.TabIndex = 6;
-            label1.Text = "Modelo:";
-            // 
-            // lbldiskn
-            // 
-            lbldiskn.AutoSize = true;
-            lbldiskn.Location = new Point(79, 60);
-            lbldiskn.Name = "lbldiskn";
-            lbldiskn.Size = new Size(28, 15);
-            lbldiskn.TabIndex = 5;
-            lbldiskn.Text = "disk";
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblCPUN = new System.Windows.Forms.Label();
+            this.lblCPU = new System.Windows.Forms.Label();
+            this.progressBarcpu = new System.Windows.Forms.ProgressBar();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbltotalram = new System.Windows.Forms.Label();
+            this.labelrampercent = new System.Windows.Forms.Label();
+            this.lblRAM = new System.Windows.Forms.Label();
+            this.progressBarRAM = new System.Windows.Forms.ProgressBar();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lbldiskn = new System.Windows.Forms.Label();
+            this.lblDisk = new System.Windows.Forms.Label();
+            this.lblDiskRead = new System.Windows.Forms.Label();
+            this.lblDiskWrite = new System.Windows.Forms.Label();
+
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(panel3);
-            Controls.Add(panel2);
-            Controls.Add(panel1);
-            Name = "Form1";
-            Text = "Monitor de Recursos";
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
-            panel2.ResumeLayout(false);
-            panel2.PerformLayout();
-            panel3.ResumeLayout(false);
-            panel3.PerformLayout();
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 370);
+            this.Text = "Monitor de Recursos";
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Size = new System.Drawing.Size(250, 110);
+            this.panel1.Controls.Add(this.lblCPUN);
+            this.panel1.Controls.Add(this.lblCPU);
+            this.panel1.Controls.Add(this.progressBarcpu);
+
+            // 
+            // lblCPUN
+            // 
+            this.lblCPUN.Text = "CPU";
+            this.lblCPUN.Font = new System.Drawing.Font("Segoe UI", 9F, FontStyle.Bold);
+            this.lblCPUN.Location = new System.Drawing.Point(10, 10);
+            this.lblCPUN.Size = new System.Drawing.Size(230, 20);
+
+            // 
+            // lblCPU
+            // 
+            this.lblCPU.Text = "Uso CPU: 0%";
+            this.lblCPU.Location = new System.Drawing.Point(10, 35);
+            this.lblCPU.Size = new System.Drawing.Size(200, 20);
+
+            // 
+            // progressBarcpu
+            // 
+            this.progressBarcpu.Location = new System.Drawing.Point(10, 65);
+            this.progressBarcpu.Size = new System.Drawing.Size(220, 20);
+            this.progressBarcpu.ForeColor = System.Drawing.Color.SteelBlue;
+
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(10, 130);
+            this.panel2.Size = new System.Drawing.Size(250, 110);
+            this.panel2.Controls.Add(this.lblRAM);
+            this.panel2.Controls.Add(this.labelrampercent);
+            this.panel2.Controls.Add(this.lbltotalram);
+            this.panel2.Controls.Add(this.progressBarRAM);
+
+            // 
+            // lblRAM
+            // 
+            this.lblRAM.Text = "RAM Disponible: 0 MB";
+            this.lblRAM.Location = new System.Drawing.Point(10, 10);
+            this.lblRAM.Size = new System.Drawing.Size(230, 20);
+
+            // 
+            // labelrampercent
+            // 
+            this.labelrampercent.Text = "0.00% usada";
+            this.labelrampercent.Location = new System.Drawing.Point(10, 30);
+            this.labelrampercent.Size = new System.Drawing.Size(100, 20);
+
+            // 
+            // lbltotalram
+            // 
+            this.lbltotalram.Text = "RAM Total: 0 MB";
+            this.lbltotalram.Location = new System.Drawing.Point(10, 50);
+            this.lbltotalram.Size = new System.Drawing.Size(200, 20);
+
+            // 
+            // progressBarRAM
+            // 
+            this.progressBarRAM.Location = new System.Drawing.Point(10, 75);
+            this.progressBarRAM.Size = new System.Drawing.Size(220, 20);
+            this.progressBarRAM.ForeColor = System.Drawing.Color.MediumPurple;
+
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(10, 250);
+            this.panel3.Size = new System.Drawing.Size(250, 110);
+            this.panel3.Controls.Add(this.lbldiskn);
+            this.panel3.Controls.Add(this.lblDisk);
+            this.panel3.Controls.Add(this.lblDiskRead);
+            this.panel3.Controls.Add(this.lblDiskWrite);
+
+            // 
+            // lbldiskn
+            // 
+            this.lbldiskn.Text = "Disco";
+            this.lbldiskn.Font = new System.Drawing.Font("Segoe UI", 9F, FontStyle.Bold);
+            this.lbldiskn.Location = new System.Drawing.Point(10, 10);
+            this.lbldiskn.Size = new System.Drawing.Size(230, 20);
+
+            // 
+            // lblDisk
+            // 
+            this.lblDisk.Text = "Uso Disco: 0%";
+            this.lblDisk.Location = new System.Drawing.Point(10, 35);
+            this.lblDisk.Size = new System.Drawing.Size(200, 20);
+
+            // 
+            // lblDiskRead
+            // 
+            this.lblDiskRead.Text = "Lectura: 0 MB/s";
+            this.lblDiskRead.Location = new System.Drawing.Point(10, 55);
+            this.lblDiskRead.Size = new System.Drawing.Size(200, 20);
+
+            // 
+            // lblDiskWrite
+            // 
+            this.lblDiskWrite.Text = "Escritura: 0 MB/s";
+            this.lblDiskWrite.Location = new System.Drawing.Point(10, 75);
+            this.lblDiskWrite.Size = new System.Drawing.Size(200, 20);
+
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+
+            // Add panels to Form
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
         }
 
         #endregion
 
-        private Label lblCPU;
-        private Label lblRAM;
-        private Label lblDisk;
-        private Label lblDiskRead;
-        private Label lblDiskWrite;
         private System.Windows.Forms.Timer timer1;
-        private Panel panel1;
-        private Panel panel2;
-        private Panel panel3;
-        private ProgressBar progressBarcpu;
-        private ProgressBar progressBarRAM;
-        private Label labelrampercent;
-        private Label lbltotalram;
-        private Label lblCPUN;
-        private Label lbldiskn;
-        private Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblCPUN;
+        private System.Windows.Forms.Label lblCPU;
+        private System.Windows.Forms.ProgressBar progressBarcpu;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbltotalram;
+        private System.Windows.Forms.Label labelrampercent;
+        private System.Windows.Forms.Label lblRAM;
+        private System.Windows.Forms.ProgressBar progressBarRAM;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lbldiskn;
+        private System.Windows.Forms.Label lblDisk;
+        private System.Windows.Forms.Label lblDiskRead;
+        private System.Windows.Forms.Label lblDiskWrite;
+
+        // You'll need to add the timer1_Tick event handler in your main code
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Your update logic here
+        }
     }
 }
